cbefd28108ce70324b0aa92f579b34fb1d833be7
Mr. Creeper <147925587+SanyaN4@users.noreply.github.com>
2025-11-30 13:23:02 +0200

    Update laucher.py

--- laucher.py
+++ laucher.py
@@ -1,4 +1,5 @@
 from customtkinter import *
+
 class ConnectWindow(CTk):
     def __init__(self):
         super().__init__()
@@ -10,17 +11,21 @@ class ConnectWindow(CTk):
         self.title("Agario Launcher")
         self.geometry("300x400")
 
-        CTkLabel(self, Text="Connect to server:", font=("Comic Sans MS", 20, "bold")).pack(pady=15, padx=20, anchor="w")
+        CTkLabel(self, text="Connect to server:", font=("Comic Sans MS", 20, "bold")).pack(pady=15, padx=20, anchor="w")
 
-        self.name_entry = CTkEntry(self, placeholder_text="Введіть ім'я: ", height=50)
+        self.name_entry = CTkEntry(self, placeholder_text="Введіть ім'я:", height=50)
         self.name_entry.pack(padx=20, anchor="w", fill="x")
-        self.host_entry = CTkEntry(self, placeholder_text="Введіть хост: ", height=50)
+
+        self.host_entry = CTkEntry(self, placeholder_text="Введіть хост:", height=50)
         self.host_entry.pack(padx=20, anchor="w", fill="x")
-        self.port_entry = CTkEntry(self, placeholder_text="Введіть порт: ", height=50)
+
+        self.port_entry = CTkEntry(self, placeholder_text="Введіть порт:", height=50)
         self.port_entry.pack(padx=20, anchor="w", fill="x")
+
         CTkButton(self, text="Приєднатися", command=self.open_game, height=50).pack(pady=15, padx=20, fill="x")
+
     def open_game(self):
         self.name = self.name_entry.get()
         self.host = self.host_entry.get()
         self.port = self.port_entry.get()
-        self.destroy
+        self.destroy()
